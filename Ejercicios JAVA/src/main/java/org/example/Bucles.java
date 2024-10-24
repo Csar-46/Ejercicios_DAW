c2acd92655859a8e6c6efe5a8dfddeb055893399
PATATASS\Patatas\Programacion <septiembrecesar@hotmail.com>
2024-10-24 14:49:34 +0200

    Practica 2 terminada.

--- Ejercicios JAVA/src/main/java/org/example/Bucles.java
+++ Ejercicios JAVA/src/main/java/org/example/Bucles.java
@@ -77,61 +77,84 @@ public class Bucles {
 
     public void practica2 () {
 
-        Scanner entrada = new Scanner(System.in);
-
+        Scanner entrada = new Scanner(System.in); //Declaración de variables.
         System.out.println("Introduzca un ISBN: ");
         String ISBN = entrada.next();
         int inicio = 0;
         char digito;
         int suma = 0;
-        int resultado;
         int num;
-        int incognita = 0;
-        int interrogacion = 0;
+        int incognita = -1;
 
-        while (ISBN.length() != 10){
+        while (ISBN.length() != 10 || ISBN.equals("0000000000")) { //Comprobamos que el formato de entrada sea el correcto.
 
-            System.out.println("Formato erróneo. Es necesario un ISBN de 10 dígitos.");
+            if (ISBN.length() != 10) {
+                System.out.println("Formato erróneo. Es necesario un ISBN de 10 dígitos.");
+            } else {
+                System.out.println("El ISBN '0000000000' no es válido.");
+            }
             ISBN = entrada.next();
-
         }
 
-        for (int i = 10; i > 0; i--){
+        try { //Usamos aqui la estructura try-catch para que si el ISBN intrudocudo son letras salte el error.
 
-            digito = ISBN.charAt(inicio);
+            for (int i = 10; i > 0; i--) { //Primer bucle para recorrer el ISBN, encontrar la X y la ? (en caso de que las haya).
 
-            if (digito == 'X'){
-                ISBN = ISBN.replace("X", "0");
-                suma = suma + 10;
-                continue;
+                digito = ISBN.charAt(inicio);
+
+                if (digito == 'X') {
+
+                    suma += 10 * i;
+
+                } else if (digito == '?') {
+
+                    incognita = i;
+
+                } else {
+
+                    num = Integer.parseInt(String.valueOf(digito));
+                    suma += num * i;
+
+                }
+                inicio++;
             }
 
-            if (digito == '?'){
-                incognita = inicio;
-                continue;
+            if (incognita != -1) { //Cuando se detecta una ? entramos en esta condicion.
+
+                for (int interrogacion = 0; interrogacion <= 10; interrogacion++) { //Este bucle es para recorrer el valor
+
+
+                    if (interrogacion == 10 && incognita != 1) { // Si la incognita no esta en la ultima posicion no puede tener valor 10.
+                        continue;
+                    }
+
+                    int suma2 = suma + (interrogacion * incognita);
+
+                    if (suma2 % 11 == 0) { //Comprobamos que ? es un numero valido e imprimimos X si el valor es 10.
+
+                        if (interrogacion == 10) {
+                            System.out.println("El número restante es: X");
+                        } else {
+                            System.out.println("El número restante es: " + interrogacion);
+                        }
+
+                        suma += interrogacion * incognita;
+                        break;
+                    }
+                }
             }
 
-            num = Integer.parseInt(String.valueOf(digito));
-            inicio++;
+            if (suma % 11 == 0) { // Comprobamos que sea un ISBN real.
 
-            resultado = num*i;
-            suma = resultado + suma;
+                System.out.println("El ISBN es válido.");
 
+            } else {
+                System.out.println("El ISBN no es válido.");
+            }
+
+        } catch (NumberFormatException e) {
+
+            System.out.println("ERROR. El formato del ISBN es incorrecto.");
         }
-        System.out.println(incognita);
-
-        for ( ; incognita > 0 ; interrogacion++){
-
-            suma = suma + (incognita * interrogacion) ;
-            if (suma % 11 == 0){
-                System.out.println("El número restante es: " + interrogacion);
-                break;
-            }
-        }
-
-        if (suma % 11 == 0){
-            System.out.println("El ISBN es válido.");
-        } else
-            System.out.println("El ISBN no es válido.");
     }
 }
