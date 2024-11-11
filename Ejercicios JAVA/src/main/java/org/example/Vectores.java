08af17812ed8dd0c5de6895b02bb3118752f372c
PATATASS\Patatas\Programacion <septiembrecesar@hotmail.com>
2024-11-11 23:14:27 +0100

    Practica 1 de vectores.

--- Ejercicios JAVA/src/main/java/org/example/Vectores.java
+++ Ejercicios JAVA/src/main/java/org/example/Vectores.java
@@ -255,137 +255,121 @@ public class Vectores {
     }
 
     public void practica1 () {
-        Scanner entrada = new Scanner(System.in); //Declaro las variables que voy a usar.
+
+        Scanner entrada = new Scanner(System.in);
         Random aleatorio = new Random();
         int primero = aleatorio.nextInt(7);
-        int valor1 = 0;
-        int valor2 = 0;
-        int suma1 = 0;
-        int suma2 = 0;
-        String potencia1 = "";
-        String potencia2 = "";
-        int samurai1 = 0;
-        int samurai2 = 0;
+        int valor;
+        int suma;
+        String potencia;
+        String[] Vequipo1 = new String[7];
+        String[] Vequipo2 = new String[7];
         int equipo1 = 0;
         int equipo2 = 0;
         int empate = 0;
+        boolean error;  // Variable para detectar errores
+
+
+        for (int equipo = 1; equipo <= 2; equipo++) { // Pedimos las potencias de ambos equipos en un mismo bucle
+            suma = 0;                                 //Ademas la variable que declaramos en la condicion del buble nos servirá para entrar al switch case mas adelante.
+
+            while (suma != 30) {
+                System.out.print("Introduce la potencia de los 7 samuráis del equipo " + equipo + " separada por espacios: ");
+                potencia = entrada.nextLine();
+                String[] Vtemporal = potencia.split(" ");
+
+
+                if (Vtemporal.length != 7) {  // Comprobamos que introducimos la cantidad exacta de numeros
+                    System.out.println("ERROR. El equipo debe contar con 7 samuráis exactos.");
+                    continue;
+                }
+
+                suma = 0;
+                error = false;  // Utilizamos error como variable de control de errores.
+
+
+                for (int i = 0; i < Vtemporal.length; i++) { //Hacemos la suma y creamos el primer vector.
+                    try {                                    //Metemos esta parte en un try-catch para controlar los errores de formato ademas de acotar el valor de entrada.
+                        valor = Integer.parseInt(Vtemporal[i]);
+                        if (valor < 1 || valor > 24) {
+                            System.out.println("ERROR. La potencia del samurái debe estar entre 1 y 24.");
+                            error = true;
+                            break;
+                        }
+                        suma += valor;
+                    } catch (NumberFormatException e) {
+                        System.out.println("ERROR. Entrada inválida. Solo se permiten números enteros.");
+                        error = true;
+                        break;
+                    }
+                }
+
+                if (error || suma != 30) { //Se hace la suma y en caso de error entra a esta condicion y resetea la variable.
+                    if (suma != 30 && !error) {
+                        System.out.println("ERROR. La potencia total no suma 30.");
+                    }
+                    suma = 0;
+                    continue;
+                }
+
+
+                switch (equipo) { // Copiamos los valores correctos al vector correspondiente.
+                    case 1:
+                        Vequipo1 = Arrays.copyOf(Vtemporal, Vtemporal.length);
+                        System.out.println("Equipo 1 listo para el combate.");
+                        break;
+                    case 2:
+                        Vequipo2 = Arrays.copyOf(Vtemporal, Vtemporal.length);
+                        System.out.println("Equipo 2 listo para el combate.");
+                        break;
+                }
+            }
+        }
 
         System.out.println("El combate comienza con el Samurai número: " + (primero + 1));
-
-        System.out.println("!! EQUIPO 1 ¡¡");
-        while (suma1 != 30) {
-            System.out.print("Introduce la potencia de los 7 samuráis separada por espacios: ");
-            potencia1 = entrada.nextLine();
-
-            String[] Vequipo1 = potencia1.split(" "); //Comprobamos que se introduzcan 7 caracteres.
-            if (Vequipo1.length != 7) {
-                System.out.println("ERROR. El equipo debe contar con 7 samuráis exactos.");
-                suma1 = 0;
-                continue;
-            }
-
-            suma1 = 0;
-            for (int i = 0; i < Vequipo1.length; i++) {
-                try {
-                    valor1 = Integer.parseInt(Vequipo1[i]);
-                    if (valor1 < 1 || valor1 > 24) {
-                        System.out.println("ERROR. La potencia del samurái debe estar entre 1 y 24.");
-                        suma1 = 0;
-                        break;
-                    }
-                    suma1 += valor1;
-                } catch (NumberFormatException e) {
-                    System.out.println("ERROR. Entrada inválida. Solo se permiten números enteros.");
-                    suma1 = 0;
-                    break;
-                }
-            }
-
-            if (suma1 != 30 && suma1 != 0) {
-                System.out.println("ERROR. La potencia total no suma 30.");
-                suma1 = 0;
-            }
-        }
-        System.out.println("Equipo 1 listo para el combate.");
-
-        System.out.println("!! EQUIPO 2 ¡¡");
-        while (suma2 != 30) {
-            System.out.print("Introduce la potencia de los 7 samuráis separada por espacios: ");
-            potencia2 = entrada.nextLine();
-
-            String[] Vequipo2 = potencia2.split(" ");
-            if (Vequipo2.length != 7) {
-                System.out.println("ERROR. El equipo debe contar con 7 samuráis exactos.");
-                suma2 = 0;
-                continue;
-            }
-
-            suma2 = 0;
-            for (int i = 0; i < Vequipo2.length; i++) {
-                try {
-                    valor2 = Integer.parseInt(Vequipo2[i]);
-                    if (valor2 < 1 || valor2 > 24) {
-                        System.out.println("ERROR. La potencia del samurái debe estar entre 1 y 24.");
-                        suma2 = 0;
-                        break;
-                    }
-                    suma2 += valor2;
-                } catch (NumberFormatException e) {
-                    System.out.println("ERROR. Entrada inválida. Solo se permiten números enteros.");
-                    suma2 = 0;
-                    break;
-                }
-            }
-
-            if (suma2 != 30 && suma2 != 0) {
-                System.out.println("ERROR. La potencia total no suma 30.");
-                suma2 = 0;
-            }
-        }
-        System.out.println("Equipo 2 listo para el combate.");
 
         System.out.println("!QUE COMIENCE EL COMBATE¡");
         System.out.println("La batalla empieza con el Samurai " + (primero + 1) + ".");
 
-        String[] Vequipo1 = potencia1.split(" ");
-        String[] Vequipo2 = potencia2.split(" ");
 
         for (int i = 0; i < Vequipo1.length; i++) {
-            if (equipo1 >= 4 || equipo2 >= 4) {
+            if (equipo1 >= 4 || equipo2 >= 4) { //Ponemos esta condición para que si alguno gana 5 rondas salga del bucle.
                 break;
             }
 
-            if (primero == 7) {
+            if (primero == 7) { //Cuando el septimo samurai pelea se resetea la variable para que salga el primero.
                 primero = 0;
             }
 
-            samurai1 = Integer.parseInt(Vequipo1[primero]);
-            samurai2 = Integer.parseInt(Vequipo2[primero]);
+            int samurai1 = Integer.parseInt(Vequipo1[primero]); //Con las lineas de abajo mostramos por pantalla quien se pelea y se suma 1 al contador del ganador.
+            int samurai2 = Integer.parseInt(Vequipo2[primero]);
 
             System.out.print("Samurai " + (primero + 1) + ".");
             if (samurai1 > samurai2) {
-                System.out.println(" > Gana el equipo 1. " + samurai1 + " vs " + samurai2);
+                System.out.println(" Gana el equipo 1. " + samurai1 + " vs " + samurai2);
                 Vequipo2[primero] = String.valueOf(0);
                 equipo1++;
             } else if (samurai1 < samurai2) {
-                System.out.println(" > Gana el equipo 2. " + samurai1 + " vs " + samurai2);
+                System.out.println(" Gana el equipo 2. " + samurai1 + " vs " + samurai2);
                 Vequipo1[primero] = String.valueOf(0);
                 equipo2++;
             } else {
-                System.out.println(" > Los samuráis se han matado entre sí. Empate. " + samurai1 + " vs " + samurai2);
+                System.out.println(" Los samuráis se han matado entre sí. Empate. " + samurai1 + " vs " + samurai2);
                 empate++;
             }
             primero++;
         }
 
-        if (equipo1 > equipo2) {
-            System.out.println("> !EQUIPO 1 GANA¡ Equipo 2 ha tenido " + equipo1 + " bajas.");
+
+        if (equipo1 > equipo2) { //Aqui comparamos los contadores que hemos acumulado antes para imprimir el resultado.
+            System.out.println("!EQUIPO 1 GANA¡ Equipo 2 ha tenido " + equipo1 + " bajas.");
         } else if (equipo1 < equipo2) {
-            System.out.println("> !EQUIPO 2 GANA¡ Equipo 1 ha tenido " + equipo2 + " bajas.");
+            System.out.println("!EQUIPO 2 GANA¡ Equipo 1 ha tenido " + equipo2 + " bajas.");
         } else {
-            System.out.println("> ¡INÉDITO! ¡HEMOS TENIDO UN EMPATE! Parece que todos los samuráis eran igual de poderosos.");
+            System.out.println("¡INÉDITO! ¡HEMOS TENIDO UN EMPATE! Parece que todos los samuráis eran igual de poderosos.");
         }
     }
 }
+
 
 
