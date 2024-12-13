d146af5967a51615f84ad66ec5c022ab41317ac9
A603\1DAW <septiembrecesar@hotmail.com>
2024-12-13 19:30:09 +0100

    ++

---
+++ Ejercicios JAVA/src/test/java/org/example/StarWarsTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StarWarsTest {
+
+    @Test
+    void comprobarDirecciones() {
+    }
+
+    @Test
+    void rellenarBordes() {
+    }
+
+    @Test
+    void rellenarPantalla() {
+    }
+}
