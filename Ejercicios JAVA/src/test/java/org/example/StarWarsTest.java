b619396985ca72a6a64ad64f8c91c7bdddb69d07
PATATASS\Patatas\Programacion <septiembrecesar@hotmail.com>
2024-12-17 14:48:56 +0100

    JavaUnit5

--- Ejercicios JAVA/src/test/java/org/example/StarWarsTest.java
+++ Ejercicios JAVA/src/test/java/org/example/StarWarsTest.java
@@ -3,17 +3,43 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class StarWarsTest {
 
     @Test
-    void comprobarDirecciones() {
-    }
-
-    @Test
     void rellenarBordes() {
+
+        String[][] pantalla = new String[5][5];
+        StarWars.rellenarBordes(pantalla);
+
+        for (int i = 0; i < pantalla.length; i++) {
+            for (int j = 0; j < pantalla[i].length; j++) {
+
+                if (i == 0 || i == 4 || j == 0 || j == 4) {
+
+                    assertEquals("#", pantalla[i][j]);
+
+                } else {
+
+                    assertEquals(" ", pantalla[i][j]);
+
+                }
+            }
+        }
     }
 
     @Test
     void rellenarPantalla() {
+
+        String[][] pantalla = new String[5][10];
+        StarWars.rellenarBordes(pantalla);
+
+        String[] direcciones = {"I", "S", "I", "B"};
+        StarWars.rellenarPantalla(pantalla, direcciones);
+        
+        assertEquals("_", pantalla[3][1]);
+        assertEquals("/", pantalla[2][2]);
+        assertEquals("_", pantalla[2][3]);
+        assertEquals("\\", pantalla[3][4]);
     }
 }
