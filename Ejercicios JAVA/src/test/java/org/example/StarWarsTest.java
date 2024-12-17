d7f7a90e274119331e6caadbe26bcba52742aea0
A603\1DAW <septiembrecesar@hotmail.com>
2024-12-17 21:07:32 +0100

    Tomas Ineditas sin comentar

--- Ejercicios JAVA/src/test/java/org/example/StarWarsTest.java
+++ Ejercicios JAVA/src/test/java/org/example/StarWarsTest.java
@@ -2,9 +2,24 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.*;
 class StarWarsTest {
+
+    @Test
+    void direcciones() {
+
+        System.setIn(new ByteArrayInputStream("ISB".getBytes()));
+
+        String[] result = StarWars.direcciones();
+        assert result.length == 3;
+        assert result[0].equals("I");
+        assert result[1].equals("S");
+        assert result[2].equals("B");
+
+    }
 
     @Test
     void rellenarBordes() {
@@ -14,32 +29,13 @@ class StarWarsTest {
 
         for (int i = 0; i < pantalla.length; i++) {
             for (int j = 0; j < pantalla[i].length; j++) {
-
-                if (i == 0 || i == 4 || j == 0 || j == 4) {
-
-                    assertEquals("#", pantalla[i][j]);
-
+                if (i == 0 || i == pantalla.length - 1 || j == 0 || j == pantalla[i].length - 1) {
+                    assert pantalla[i][j].equals("#");
                 } else {
-
-                    assertEquals(" ", pantalla[i][j]);
-
+                    assert pantalla[i][j].equals(" ");
                 }
             }
         }
-    }
 
-    @Test
-    void rellenarPantalla() {
-
-        String[][] pantalla = new String[5][10];
-        StarWars.rellenarBordes(pantalla);
-
-        String[] direcciones = {"I", "S", "I", "B"};
-        StarWars.rellenarPantalla(pantalla, direcciones);
-        
-        assertEquals("_", pantalla[3][1]);
-        assertEquals("/", pantalla[2][2]);
-        assertEquals("_", pantalla[2][3]);
-        assertEquals("\\", pantalla[3][4]);
     }
 }
